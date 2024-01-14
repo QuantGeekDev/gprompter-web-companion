e3c9aad1c02f31cd1efe8b7ba702accd7010a2e0
Alex Andru <alex007d@gmail.com>
2024-01-14 18:24:59 +0100

    Add Prompts page

---
+++ src/app/prompts/new/page.tsx
@@ -0,0 +1,12 @@
+import PromptForm from "@/components/PromptForm/PromptForm";
+
+const NewPrompt = (): React.ReactElement => {
+  return (
+    <section className="flex flex-col p-5">
+      <h1 className="text-main text-4xl text-center"> Add New Prompt</h1>
+      <PromptForm />
+    </section>
+  );
+};
+
+export default NewPrompt;
